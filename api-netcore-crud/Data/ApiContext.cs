48e13a7cf8863176c6ff7592ba0cd2517d1039cd
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-05-12 22:00:51 -0300

    se borran archivos de la plantilla y se add apicontext

---
+++ api-netcore-crud/Data/ApiContext.cs
@@ -0,0 +1,19 @@
+﻿using api_netcore_crud.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_netcore_crud.Data
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pais> Paises { get; set; }
+    }
+}
--- api-netcore-crud/Data/Entities/Pais.cs
+++ api-netcore-crud/Data/Entities/Pais.cs
@@ -7,5 +7,8 @@ namespace api_netcore_crud.Data.Entities
 {
     public class Pais
     {
+        public int Id { get; set; }
+        public string CodPais { get; set; }
+        public string NomPais { get; set; }
     }
 }
