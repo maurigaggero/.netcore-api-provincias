6433f4691064dd5853802e00aab526fdfdd52f18
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-05-12 22:25:37 -0300

    Creamos la db a traves de entity

---
+++ api-netcore-crud/Migrations/20200513011840_inico.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api_netcore_crud.Migrations
+{
+    public partial class inico : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Paises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodPais = table.Column<string>(nullable: true),
+                    NomPais = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paises", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Paises");
+        }
+    }
+}
