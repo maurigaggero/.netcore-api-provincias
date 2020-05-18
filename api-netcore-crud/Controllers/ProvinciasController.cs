820b4d054b514d74368cba765165655b39cf83d3
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-05-18 17:27:08 -0300

    crea ProvinciasController

---
+++ api-netcore-crud/Controllers/ProvinciasController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_netcore_crud.Controllers
+{
+    public class ProvinciasController
+    {
+    }
+}
